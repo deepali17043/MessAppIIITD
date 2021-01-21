17053c34c6e5311cb452834bdb2c23e7d78af10c
Deepali Chawla <Deepali@Deepalis-MacBook-Pro.local>
2021-01-21 19:07:26 +0530

    static_root set

--- Aahar/settings.py
+++ Aahar/settings.py
@@ -136,6 +136,9 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # APPEND_SLASH = False
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
