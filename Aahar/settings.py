d98b18aa7319f64da0c3ae82daf143bb956851ff
Deepali Chawla <Deepali@Deepalis-MacBook-Pro.local>
2021-02-07 23:46:59 +0530

    debug mode off

--- Aahar/settings.py
+++ Aahar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9l(7rhajws#we8@r162r@-6u7+xktlou3c*sa$nwky+=k&#_$u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'aahar-iiitd.herokuapp.com']
 
